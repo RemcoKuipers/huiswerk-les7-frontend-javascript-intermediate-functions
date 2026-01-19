b17a0756aa797d23b7f35ebbcb271bded758e619
RemcoKuipers <remslaap@live.nl>
2026-01-19 10:06:54 +0100

    Opdracht 2a en 2b functions-and-scopes les 7 voltooid

--- functions-and-scope.js
+++ functions-and-scope.js
@@ -58,7 +58,6 @@ console.log(howManyCumLaudeOne, howManyCumLaudeTwo, howManyCumLaudeThree);
 /* 2a: Script schrijven  */
 // De studenten-administratie moet ieder blok opnieuw berekenen wat het gemiddelde eindcijfer is, maar we beginnen met de grades array van hierboven.
 // Schrijf de stapjes om dit te kunnen berekenen eerst uit en vraag jezelf de volgende dingen af:
-// Log het antwoord in de terminal.
 let averageGrades = 0;
 // * Hoe zorgt ik ervoor dat ik alle waardes uit de array kan langslopen, ook als de array wel 100 entries zou bevatten? Door een for loop te maken die met een .lenght property alle cijfers van de array doorloopt
 for (let i = 0; i < grades.length; i++) {
@@ -66,6 +65,7 @@ for (let i = 0; i < grades.length; i++) {
     averageGrades = averageGrades + grades[i];
 }
 // * Hoe wordt een gemiddelde berekend? Door alle cijfers (grades) bij elkaar op te tellen en te delen door het aantal cijfers in de array.
+// Log het antwoord in de terminal.
 console.log(averageGrades / grades.length);
 // ---- Verwachte uitkomst: 6.642857142857143
 
@@ -73,12 +73,28 @@ console.log(averageGrades / grades.length);
 /* 2b: Omschrijven tot een herbruikbare functie */
 // Schrijf een functie genaamd averageGrade, die een array van cijfers verwacht (zoals grades) en het gemiddelde cijfer teruggeeft. Gebruik hiervoor jouw antwoord van 2a.
 // Zorg ervoor dat jouw functie ook werkt als we een andere array willen checken, zoals bijvoorbeeld: [6, 4, 5] of [8, 9, 4, 6, 10].
-// Log het antwoord in de terminal.
-
+// Input : Een array van cijfers (grades)
+// Output : Gemiddeld cijfer van de array met cijfers
+// Werkwijze :
+// Stap 1 : Schrijf een functie genaamd averageGrade
+function averageGrade(grades) {
+// Stap 2 : Gebruik de antwoorden van 2a
+    let total = 0;
+    for (let i = 0; i < grades.length; i++) {
+        total = total + grades[i]
+    }
+    return total / grades.length;
+}
+// Stap 3 : Maak niet aanpasbare variabelen om te kijken wat het gemiddelde cijfer is om te kunnen loggen.
+const averageGradesOne = averageGrade(grades);
+const averageGradesTwo = averageGrade([6, 4, 5]);
+const averageGradesThree = averageGrade([8, 9, 4, 6, 10]);
 // ---- Verwachte uitkomsten:
 // averageGrade(grades) geeft 6.642857142857143
 // averageGrade([6, 4, 5]) geeft 5
 // averageGrade([8, 9, 4, 6, 10]) geeft 7.4
+// Log het antwoord in de terminal.
+console.log(averageGradesOne, averageGradesTwo, averageGradesThree);
 
 
 /* 2c: Afronden op twee decimalen */
